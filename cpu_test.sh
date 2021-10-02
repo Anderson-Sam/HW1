f09616b075d0c501e2bfe5a4f7c2fcc7a5c2e82e
Anderson-Sam <sam.anderson.02.21@gmail.com>
2021-10-02 05:27:22 +0000

    thread count added

--- cpu_test.sh
+++ cpu_test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# get user input
+echo number of threads to use
+read threads
 for i in {0..9}
 do
 	echo TEST $i
-	sysbench --test=cpu --cpu-max-prime=20000 run
+	sysbench --test=cpu --cpu-max-prime=20000 --max-time=30 --num-threads=$threads run
 done
