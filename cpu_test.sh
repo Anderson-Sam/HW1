f6eee7444fb71a2f6ece8933ce7723e48a9ea255
Anderson-Sam <sam.anderson.02.21@gmail.com>
2021-10-04 15:12:17 +0000

    fleiable testing added

--- cpu_test.sh
+++ cpu_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # get user input
-echo number of threads to use
-read threads
+echo what command do we use?
+read command
 for i in {0..9}
 do
 	echo TEST $i
-	sysbench --test=cpu --cpu-max-prime=20000 --max-time=30 --num-threads=$threads run
+	$command
 done
