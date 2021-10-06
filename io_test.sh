a06614ba58ce099c6fdac9662d1e9efd0d485359
Anderson-Sam <sam.anderson.02.21@gmail.com>
2021-10-06 00:25:20 +0000

    clear cache

--- io_test.sh
+++ io_test.sh
@@ -5,13 +5,21 @@ input=y
 yes=y
 while [[ "$input" == "$yes" ]]
 	do
-		echo what command do we use?
-		read command
+		base="sysbench --test=fileio --file-block-size=1G --file-num=5 --time=30"
+		echo Add additional flags
+		read flags
+		command=""
+		command+="$base "
+		command+="$flags "
+	        echo $command	
+		$command prepare
 		for i in {0..9}
 			do
 	        		echo TEST $i
-				$command
+				$command run
 			done
+		$command cleanup
+		sync
 		echo is there another test to run? [y/n]
 		read input
 	done
